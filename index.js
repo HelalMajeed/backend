4eb2eeb5a4cc01529d516c24aa7b47b019409e1e
Helal Majeed <helal@me.com>
2024-01-27 08:13:07 -0800

    commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.listen(53742, () => {
+    console.log("server is running at http://localhost/53742");
+});
+
+app.post("/api/add_product", (req, res)=> {
+    console.log("Result", req.body);
+    const pdata = {
+        "id": productData.length+1,
+        "pname": req.body.pname,
+        "pprice": req.body.pprice,
+        "pdesc": req.body.pdesc,
+    }
+
+    productData.push(pdata);
+    console.log("Final", pdata);
+    res.status(200).send({
+        "status_code": 200,
+        "massege": "Product add successfully",
+        "product": pdata,
+    })
+});
+    
